74fcc1d37bd2b216ff1dc634b7fee11d4c53885b
AnthonyCarroll97 <anthonycarroll1997@hotmail.com>
2020-11-06 15:05:27 +1000

    more work to navbar and burger menu, added jquery

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
-    
+    $('.burger').click(function () { 
+        if ($('navbar').hasClass('nav-visible')) {
+            $('navbar').addClass('nav-hidden');
+            $('navbar').removeClass('nav-visible')
+        } else {
+            $('navbar').addClass('nav-visible');
+            $('navbar').removeClass('nav-hidden');
+        }
+        
+    });
 });
